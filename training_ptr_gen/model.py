93232f23bebcd3039d5e3e5953f119ab41628e7a
Daniel Laszlo <daniel.laszlo94@gmail.com>
2019-10-06 02:22:07 +0200

    Fix it for real now?

--- training_ptr_gen/model.py
+++ training_ptr_gen/model.py
@@ -256,7 +256,7 @@ class Decoder(nn.Module):
         # y_t_1_embd = embedding(y_t_1)
         input_string_sequence = [
             [self.vocab._id_to_word[int(id.item())].decode("utf-8")]
-            if y_t_1 < self.vocab.size() else [0]
+            if y_t_1 < self.vocab.size() else self.vocab._id_to_word[0].decode("utf-8")
             for id in y_t_1
         ]
 
